0e97328a910fedcd5110ae72d707966f6f3f8103
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-27 14:54:17 +0530

    Create credentials.py

---
+++ APIs/SOAP_API/credentials.py
@@ -0,0 +1,13 @@
+print("*****\nBOT: Enter the D7 SMS SOAP-API CREDENTIALS, as stated below...\n*****")
+
+USERNAME = input("\nBOT: PLease enter your D7 SOAP API-Username :-\t")
+print("USERNAME:\t",USERNAME)
+
+PWD = input("\nBOT: PLease enter your D7 SOAP API-Password :-\t")
+print("PASSWORD:\t",PWD)
+
+print("\n\nThank You.")
+
+
+
+@ CODED BY TSG405, 2020
