c8b6b07f65133ec756e0049613f34c68240ff215
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-27 15:47:19 +0530

    Create credentials.py

---
+++ APIs/TWILIO/credentials.py
@@ -0,0 +1,14 @@
+print("*****\nBOT: Enter the TWILIO API CREDENTIALS, as stated below...\n*****")
+
+SID = input("\nBOT: PLease enter your TWILIO Account-SID :-\t")
+print("ACC SID:\t",SID)
+
+TOKEN = input("\nBOT: PLease enter your TWILIO Account AUTH-Token :-\t")
+print("AUTH Token:\t",TOKEN)
+
+sender= input("\nBOT: Type your (sender's) PHONE--NUMBER [with the country-code] e.g -:- +919xxxxxxxxx (IND): \t")
+print("\n\nThank You.")
+
+
+
+@ CODED BY TSG405, 2020
