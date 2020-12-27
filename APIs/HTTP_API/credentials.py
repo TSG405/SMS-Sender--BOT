bd1268ca60bca571b88ad6fb2d4217ea2b83e0c0
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-27 14:08:36 +0530

    Create credentials.py

---
+++ APIs/HTTP_API/credentials.py
@@ -0,0 +1,13 @@
+print("*****\nBOT: Enter the D7 SMS-HTTP API CREDENTIALS, as stated below...\n*****")
+
+USERNAME = input("\nBOT: PLease enter your D7 HTTP API-Username :-\t")
+print("USERNAME:\t",USERNAME)
+
+PWD = input("\nBOT: PLease enter your D7 HTTP API-Password :-\t")
+print("PASSWORD:\t",PWD)
+
+print("\n\nThank You.")
+
+
+
+@ CODED BY TSG405, 2020
