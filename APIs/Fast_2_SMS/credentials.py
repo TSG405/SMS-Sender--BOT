47e99e2c143b80a4a9c74fe69a8bdf30354aa8d8
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-27 15:22:08 +0530

    Create credentials.py

---
+++ APIs/Fast_2_SMS/credentials.py
@@ -0,0 +1,10 @@
+print("*****\nBOT: Enter the FAST-2-SMS API CREDENTIALS, as stated below...\n*****")
+
+AUTH = input("\nBOT: PLease enter your FAST-2-SMS API-Authorization :-\t")
+print("Authorization ID:\t",AUTH)
+
+print("\n\nThank You.")
+
+
+
+@ CODED BY TSG405, 2020
