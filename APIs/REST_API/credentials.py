277d1f4875f2548dca5417ce00123d940e910530
Tanmoy Sengupta <65300045+TSG405@users.noreply.github.com>
2020-12-27 14:42:53 +0530

    Create credentials.py

---
+++ APIs/REST_API/credentials.py
@@ -0,0 +1,10 @@
+print("*****\nBOT: Enter the D7 SMS REST-API CREDENTIALS, as stated below...\n*****")
+
+AUTH = input("\nBOT: PLease enter your D7 REST API-Authorization :-\t")
+print("Authorization ID:\t",AUTH)
+
+print("\n\nThank You.")
+
+
+
+@ CODED BY TSG405, 2020
